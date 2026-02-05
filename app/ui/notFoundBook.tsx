6bc3393b2c1b87dee97f36cd3f5a3b4fee2117f1
medi17 <medanabebe17@gmail.com>
2026-02-05 23:15:05 +0300

    added not found pages

---
+++ app/ui/notFoundBook.tsx
@@ -0,0 +1,23 @@
+
+
+export default function NotFoundBook() {
+    return(
+        <div className="flex justify-center min-w-full  ">
+            <div className="flex  flex-col items-center justify-center  bg-black/10 text-gray-400 px-6 py-5">
+                <h1 className="text-[120px] font-extrabold leading-none tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-[#238E8E] via-[#ffffff] to-[#238E8E]">
+                    404
+                </h1>
+                <p className="text-xl sm:text-2xl font-semibold my-10  ">Book Not Found</p>
+                <p className="text-center max-w-md text-sm sm:text-base mb-8">
+                    The book you're looking for doesn’t exist or has been moved. Let’s get you back on track.
+                </p>
+                <a
+                    href="/"
+                    className="fancyBorder px-6 py-3 bg-[#238E8E] text-white rounded-full shadow hover:bg-[#1b6f6f] transition duration-300"
+                >
+                    Go Home
+                </a>
+            </div>
+        </div>
+    )
+}
