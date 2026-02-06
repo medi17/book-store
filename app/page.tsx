ecd4c0426a972b5b817d56aa513dcae8fc45c7e0
medi17 <medanabebe17@gmail.com>
2026-02-06 23:52:42 +0300

    updated the main home

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,19 @@
 "use client";
-import Image from "next/image";
-import IntroText from "@/app/ui/introText";
-import Search from "@/app/ui/search";
-import Link from "next/dist/client/link";
-import RecentlyPublishedBooks from "./ui/recentlypub-books";
-import Header from "@/app/ui/header";
-import BookoftheMonth from "./ui/BOTM";
-import MyShelf from "./pages/Myshelf/page";
-import BookView from "./pages/BookView/[id]/page";
-import Profile from "./pages/Profile/page";
-import Login from "@/app/ui/login";
-import SignUp from "@/app/ui/signUp";
+
 import "@/app/globals.css";
-import Script from "next/script";
-import Providers from "./queryProvider";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { useSearchBooks } from "@/app/hooks/useSearchBooks";
-import SearchResults from "./ui/searchResult";
+import AnimatedHome from "./ui/animatedHome";
 
 export default function Home() {
   const searchParams = useSearchParams();
-  const q = searchParams.get("q") || "";
+  const q = searchParams.get("q") || ""
 
   const { data: books, isLoading, error } = useSearchBooks(q);
 
   return (
     <div className="flex flex-col min-h-screen bg-black/20">
-      <IntroText />
-      <Search placeholder="Search..." isLoading={isLoading} />
-
-      <main className="grow">
-        {q ? (
-          <SearchResults books={books} />
-        ) : (
-          <>
-            <RecentlyPublishedBooks />
-            <BookoftheMonth />
-          </>
-        )}
-      </main>
+      <AnimatedHome isLoading={isLoading} q={q} books={books}/>
     </div>
   );
 }
