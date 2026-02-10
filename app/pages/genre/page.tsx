3ff8be126b5d08a50923e9d4ed88a2eee37d09d7
medi17 <medanabebe17@gmail.com>
2026-02-10 23:52:51 +0300

    integrated genre page

--- app/pages/genre/page.tsx
+++ app/pages/genre/page.tsx
@@ -1,16 +1,44 @@
-import {Book} from '../../lib/definition';
+'use client'
+
+import { allGenres, genresWithbooks } from '../../lib/definition';
 import '../../globals.css';
-import { fetchBooks } from '@/app/lib/fetching-data';
-import {handlee, gabarito} from "@/app/ui/fonts";
-import Link from "next/link";
-import {topRatedbooks} from "@/app/lib/fetching-data"
-import {books, TopRatedbooks} from "@/app/lib/placeholder-data"
-import Search from '../../ui/searchForPages';
-import Image from 'next/image'
-import Genre from './genre';
-const bookGenres = ['Fantasy', 'Science ', 'Romance', 'Mystery', 'Thriller', 'Horror', 'Historical', 'Adventure', 'Literary Fiction', 'Young Adult', 'Children\'s Books', 'Contemporary Fiction', 'Graphic Novels', 'Dystopian', 'Paranormal', 'Biography & Memoir', 'Self-Help', 'Health & Wellness', 'True Crime', 'History', 'Business & Finance', 'Psychology', 'Philosophy', 'Religion & Spirituality', 'Politics', 'Travel', 'Cookbooks', 'Essays & Journalism'];
+import SearchForGenres from './searchForGenres';
+import BrowseGenres from '@/app/pages/genre/browseGenres';
+import { useQuery } from '@tanstack/react-query';
+import { fetchAllGenres } from '@/app/lib/fetching-data';
+import GenreCategory from './genreCatagory';
 
-export default function Page(){
-    return <Genre books={books}/>
+
+
+export default function Genre() {
+    
+    const {data: genresWbooks, isLoading, error} = useQuery<allGenres, Error>({
+        queryKey:["genres"],
+        queryFn: fetchAllGenres
+    })
+
+    const allGeners = genresWbooks ?? []
+
+    return (
+        <div className="w-fit mx-auto mt-5">
+            <h2 className="text-2xl font-bold mt-8 mx-6 text-[#238E8E] ">Genres</h2>
+            <div className="flex flex-col lg:flex-row lg:justify-between lg:gap-8 w-full p-4 sm:w-fit">
+                <div className={"font-gabarito flex flex-col w-full lg:w-fit"}>
+                    <SearchForGenres placeholder="Search..." />
+                    <div>
+                        { 
+                            allGeners.map((genres: genresWithbooks, i) => (
+                                <GenreCategory key= {i} genres = {genres}/>
+                            ))
+                        }
+                    </div>
+
+                </div>
+                <div className="w-fit mt-8 lg:mt-0">
+                    <h1 className="font-bold border-b-2 border-gray-600 pb-4 mb-4 lg:mb-1">Browse</h1>
+                    <BrowseGenres />
+                </div>
+            </div>
+        </div>
+    )
 }
-
