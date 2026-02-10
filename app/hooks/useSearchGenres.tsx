00870050c9999732083e2cf5491e042a9c732a3f
medi17 <medanabebe17@gmail.com>
2026-02-10 23:42:23 +0300

    updated this hook

--- app/hooks/useSearchGenres.tsx
+++ app/hooks/useSearchGenres.tsx
@@ -1,13 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
+import { genreDetails } from '../lib/definition';
 
-export const useSearchGenres = (query: string) => {
-    return useQuery({
-        queryKey: ['genres', 'search', query],
+export const useSearchGenres = (genreName: string) => {
+    return useQuery<genreDetails>({
+        queryKey: ['genres', 'search', genreName],
+        enabled: !!genreName,
         queryFn:async ()=>{
       
-            if (!query) return [];
+            if (!genreName) return [];
 
-            const res = await fetch(`http://localhost:3000/api/genres/search?q=${encodeURIComponent(query)}`, {
+            const res = await fetch(`http://localhost:3000/api/genres/search?genreName=${encodeURIComponent(genreName)}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
