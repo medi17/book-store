ec4486ee6893e25951d6b29bc23841b91a5ab5ac
medi17 <medanabebe17@gmail.com>
2026-02-13 15:17:33 +0300

    added date changer

---
+++ app/helper/date.ts
@@ -0,0 +1,17 @@
+export function changeDate(dateString: string) {
+  return new Date(dateString).toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
+
+export function onlyDate(dateString: string) {
+  return new Date(dateString).toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric"
+  });
+}
