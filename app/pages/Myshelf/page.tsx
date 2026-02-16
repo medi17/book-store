648c78b2fd11947866da07bb5c428d7cc4c39869
medi17 <medanabebe17@gmail.com>
2026-02-16 23:58:50 +0300

    updated the my shelf page

--- app/pages/Myshelf/page.tsx
+++ app/pages/Myshelf/page.tsx
@@ -1,12 +1,10 @@
 "use client"
-import { Myshelf } from "@/app/lib/shelf";
-import { Book, fetchBook, bookDetailSchema } from "@/app/lib/definition";
-import { StarIcon } from "@heroicons/react/24/outline";
-import  {useShelfStore}  from "@/store/shelfStore";
-import { useState, useEffect } from "react";
-import Link from "next/dist/client/link";
+
+import { bookDetailSchema } from "@/app/lib/definition";
 import { useQuery } from '@tanstack/react-query';
 import { fetchShelfBooks } from "@/app/lib/fetching-data";
+import Rating from "../../helper/rating";
+import { onlyYear } from "@/app/helper/date";
 
 
 export default function MyShelf(){
@@ -21,25 +19,27 @@ export default function MyShelf(){
         retry: false,
     });
 
-    return(
-        <div className="w-full  flex flex-col justify-center mx-auto ">
-            <h1 className="font-bold text-2xl my-14 ml-2 sm:ml-10 text-cyan-600">My Shelf</h1>
-            <div className="flex flex-col md:flex-row md:flex-wrap  gap-10 justify-center items-center w-full">
+    if (isLoading){ return <p>Loading shelf...</p>}
 
-                {shelf?.map((book: bookDetailSchema ) =>(
-                    <div key={book.name} className="flex flex-col md:flex-row gap-8 max-w-[280px] px-5 py-3  md:min-w-[480px] bg-black/100 border-[0.15] border-gray-700">
+    if (error) return <p>Failed to load shelf</p>;
+
+    return(
+        <div className="w-full  flex flex-col justify-center ">
+            <h1 className="font-bold text-2xl my-8 px-5 sm:ml-10 text-cyan-600">My Shelf</h1>
+            <div className="flex flex-col md:grid md:grid-cols-2 gap-5 md:px-15 justify-between items-center w-full">
+
+                {shelf?.map((book: bookDetailSchema) =>(
+                    <div key={book.id} className="flex flex-col md:flex-row md:items-center gap-8 max-w-[480px] px-5 py-3 bg-black border-[0.15] border-gray-700 rounded-lg">
                         <div className="">
                             <img src={book.image} alt="" className="w-[90px] h-[135px] "/>
                         </div>
-                        <div className="flex flex-col flex-grow-0 gap-1 w-fit">
-                            <p className=""><span className="text-gray-500 mr-3">title: </span>{book.name}</p>
-                            <p className=""><span className="text-gray-500 mr-3">author: </span>{book?.authors?.map((author: {id: string; name: string}) => (<span key={author.id}>{author.name}</span>))}</p>
-                            <p className=""><span className="text-gray-500 mr-3">published: </span>{book?.publishers?.map((pub: {id: string; name: string}) => (<span key={pub.id}>{pub.name}</span>))}</p>
-                            <span className="flex"><span className="text-gray-500 mr-3">rating: </span>{
-                            [...Array(Math.round(5))].map((_, i) =>(
-                                <StarIcon key={i} className="w-[24px] h-[24px]" />
-                            ))}</span>
-                            <button className="fancyBorder rounded-lg flex-grow-0 sm:px-6 mt-3" >Mark as read</button>
+                        <div className="flex flex-col grow-0 gap-1 w-fit">
+                            <p className=""><span className="text-gray-500 mr-1">Title: </span>{book.name}</p>
+                            <p className=""><span className="text-gray-500 mr-1">Author: </span>{book.authors.map((author) => author.name)}</p>
+                            <p className=""><span className="text-gray-500 mr-1">Published: </span>{onlyYear(book.firstPublishedDate)}</p>
+                            <div className="flex"><span className="text-gray-500 mr-1">Rating: </span> 
+                                <Rating rateNum={book.averageRating} className='w-6'/>
+                            </div>
                         </div>
                     </div>
                 ) )}
